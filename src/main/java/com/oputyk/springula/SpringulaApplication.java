7af087f1ed5a92b20a99fe76c789954f78701f08
Kamil Cegliński <kamil.ceglinski@primaris.eu>
2018-09-03 10:21:31 +0200

    Init

---
+++ src/main/java/com/oputyk/springula/SpringulaApplication.java
@@ -0,0 +1,12 @@
+package com.oputyk.springula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringulaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringulaApplication.class, args);
+    }
+}
